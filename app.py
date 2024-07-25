836d04f4d1a1e9bf2cff4677a496c502dfcc7ae7
unknown <marcosdzarate@gmail.com>
2024-07-25 11:08:48 -0300

    modified with new promp

--- app.py
+++ app.py
@@ -18,19 +18,32 @@ settings = {
 
 }
 
-with open('./response_1720786934558_data.json', 'r') as f:
+
+with open('/response_1720786934558_schema.json', 'r', encoding="utf8") as f:
+    json_schema = json.load(f)
+
+with open('/response_1720786934558_data.json', 'r', encoding="utf8") as f:
     json_data = json.load(f)
+
 
 @cl.on_message
 async def on_message(message: cl.Message):
     response = await client.chat.completions.create(
         messages=[
             {
-                "content": "You are a helpful bot, you always reply in Spanish",
+                "content": """You are a chatbot named "GBIBot" designed to assist users with datasets published in the Global Biodiversity Information Facility (GBIF).
+                Your expertise is focused solely on providing information about biodiversity datasets via GBIF. This includes metadata, institutions, locations, person, 
+                dates, and general queries related to the metadata associated with the datasets. You do not provide information outside this scope. 
+                If a question is not about biodiversity data, please reply: "I specialize only in queries related to biodiversity data from GBIF." 
+                Always reply in Spanish""",
                 "role": "system"
             },
             {
-                "content": f"Tienes un archivo JSON con el siguiente contenido: {json.dumps(json_data)}",
+                "content": f"Tienes un archivo JSON con los siguientes datos: {json.dumps(json_data)}",
+                "role": "system"
+            },
+            {
+                "content": f"Tienes un archivo JSON schema describiendo los datos: {json.dumps(json_schema)}",
                 "role": "system"
             },
             {
